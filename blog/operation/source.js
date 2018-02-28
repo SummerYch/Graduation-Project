e0fc7cec72b49af6ac264c38daea57ead45acd92
SummerYch <296635947@qq.com>
2018-02-28 08:58:19 +0800

    文件上传保存至服务器

--- blog/operation/source.js
+++ blog/operation/source.js
@@ -1,7 +1,34 @@
 const db = require('../db/db');
+var formidable = require('formidable');
+var path = require("path");
+var fs = require("fs");
 
-function source(req,res){
-    console.log(req.body);
+function source(req, res) {
+    var form = new formidable.IncomingForm(); //创建上传表单  
+    form.encoding = 'utf-8'; //设置编辑  
+    form.uploadDir = './file'; //设置上传目录  
+    form.keepExtensions = false; //保留后缀 
+    form.parse(req, function (err, fields, files) {
+        if (err) {
+            res.send(err);
+            return;
+        }
+        if (fields.action == 'upload') {
+            upload(req, res, fields, files);
+        }
+    });
 }
-
+function upload(req, res, fields, files) {
+    fs.renameSync(files.file.path, "./file/" + files.file.name);
+    var filepath = './file/'+files.file.name;
+    var userid = fields.userid;
+    var username = fields.username;
+    var uploadtime = fields.uploadtime;
+    var keyword = fields.keyword;
+    var description = fields.description;
+    var filetype = fields.filetype;
+    var classify = fields.classify;
+    var point = fields.point;
+    res.send("success");
+}
 module.exports = source;
--- blog/public/javascripts/uploadsource.js
+++ blog/public/javascripts/uploadsource.js
@@ -1,37 +1,54 @@
 $(function () {
-    $("#file").change(function(){
-        var data = new FormData($("#uploadform")[0]);
-        data.append('file',$("#file")[0].files[0]);
-        data.append('action','upload');
-        $.ajax({
-            url:'/source/upload',
-            type:'post',
-            data:data,
-            contentType:false,
-            processData:false,
-            success:function(data){
-
-            },
-            error:function(err){
-                console.log("err:"+err);
-            }
-        })
-    });
-    $("#submit").click(function(){
-        console.log(data);
-        $.ajax({
-            url:'/source/upload',
-            type:'post',
-            data:data,
-            contentType:false,
-            processData:false,
-            success:function(data){
-
-            },
-            error:function(err){
-                console.log("err:"+err);
-            }
-        })
-    })
     
+    var userid = getUrlParameter("userid");
+    var username = getUrlParameter("username");
+    $("#file").change(function () {
+        var file = $("#file")[0].files[0];
+        var filename = file.name;
+        console.log(filename);
+        $("#filename").html(filename);
+    });
+
+    $("#submit").click(function () {
+        if ($("#source_name") == "" || $("#tag") == "" || $("#description") == "") {
+            alert("请填写相关资料");
+        } else {
+            var uploadtimeA = new Date();
+            var uploadtime = uploadtimeA.toLocaleString();
+            var sourcename = $("#source_name").val();
+            var keyword = $("#tag").val();
+            var description = $("#description").val();
+            var filetype = $("#file_type option:selected").val();
+            var classify = $("#classify option:selected").val();
+            var point = $("#fraction option:selected").val();
+            var data = new FormData();
+            var file = $("#file")[0].files[0];
+            data.append('file', file);
+            data.append('action', 'upload');
+            data.append('userid',userid);
+            data.append('username',username);
+            data.append('uploadtime',uploadtime);
+            data.append('keyword',keyword);
+            data.append('description',description);
+            data.append('filetype',filetype);
+            data.append('classify',classify);
+            data.append('point',point);
+            $.ajax({
+                url: '/source/upload',
+                type: 'post',
+                data: data,
+                contentType: false,
+                processData: false,
+                success: function (data) {
+                   if(data == 'success'){
+                       alert("文件上传成功");
+                   } 
+                },
+                error: function (err) {
+                    console.log("err:" + err);
+                }
+            });
+        }
+        return false;
+    });
 })
